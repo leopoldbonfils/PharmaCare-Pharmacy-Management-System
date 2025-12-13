a8c740f0a35afc0ac591406899a97bced0fe11bc
GATETE Bertrand <136337742+GATETE2@users.noreply.github.com>
2025-12-13 21:53:58 +0200

    Add files via upload

---
+++ PrescriptionItems.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const PrescriptionItems = () => {
+  return (
+    <div>
+      {/* PrescriptionItems component */}
+    </div>
+  );
+};
+
+export default PrescriptionItems;
+
