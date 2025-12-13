13386cdde2ec5796cd047d11cf2991099de98aad
Titus MUCYO IRIMASO <149688327+Titus-30@users.noreply.github.com>
2025-12-13 21:04:50 +0200

    Add files via upload

---
+++ CreateUserDto.cs
@@ -0,0 +1,34 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace PharmaCareSystem.Api.DTOs.Users
+{
+    public class CreateUserDto
+    {
+        [Required]
+        [StringLength(50)]
+        public string Username { get; set; } = string.Empty;
+
+        [Required]
+        [MinLength(6)]
+        public string Password { get; set; } = string.Empty;
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        public string Role { get; set; } = string.Empty;
+
+        [Required]
+        public string FirstName { get; set; } = string.Empty;
+
+        [Required]
+        public string LastName { get; set; } = string.Empty;
+
+        [Required]
+        [RegularExpression(@"^07[0-9]{8}$")]
+        public string PhoneNumber { get; set; } = string.Empty;
+
+        public string? ProfileImageUrl { get; set; }
+    }
+}
